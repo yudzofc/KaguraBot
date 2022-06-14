83a80adaf195499fbf116bb11a2673013d36c6d9
yudzofc <106936091+yudzofc@users.noreply.github.com>
2022-06-14 13:42:42 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//---------( APIKEY )---------//
+global.APIs = {
+zenz: 'https://zenzapis.xyz',
+}
+global.APIKeys = {
+'https://zenzapis.xyz': 'BagasPrdn',
+}
+
+//---------( SETTING )---------//
+global.owner = ['628569365294','6285600380823']
+global.premium = ['6285693650294','6285600380823']
+global.ownernomer = ['6282252285143','6285600380823']
+global.ownername = ['yuds','Faisal']
+global.botname = 'Yudsbotz'
+global.footer = 'Yudsbotz'
+global.thumb = fs.readFileSync('./image/fake.jpg')
+global.veloriy = fs.readFileSync('./image/kagura.mp4')
+global.sessionName = 'session'
+global.packname = '© Yudsotz  🔥  LR X ONE : 6285693650294'
+global.author = '2022'
+global.sp = '⭔'
+
+//---------( INFO )---------//
+global.youtube = '-'
+global.ig = 'https://instagram.com/lebihkerendariandy_warholl.093'
+global.mygc = 'https://chat.whatsapp.com/Hh71t3HGLE0EL22nxxCnT3'
+global.myweb = 'https://yudzofc'
+global.email = 'wahyudip093@gmail.com'
+global.region = 'spanish'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.limitawal = {
+premium: "Infinity",
+free: 20,
+}
+
+//---------( RANDOM IMAGE )---------//
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//---------( MESSAGES )---------//
+global.mess = {
+success: '*Nih bang...*',
+admin: '*Kamu bukan admin bambang!*',
+botAdmin: '*Bot Bukan Admin Tod!*',
+owner: '*Kamu bukan owner ku anj*',
+group: '*Coba fitur ini di grup*',
+private: '_Cuma buat Private Chat!_',
+bot: '_Fitur Khusus Pengguna Nomor Bot_',
+wait: '_Sabar kak..._',
+error: '_Fitur Sedang Error!_',
+endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+}
+
+//---------( PEMBATAS )---------//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
